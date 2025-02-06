40a824efcefc430c71b41042eee1bda5aa800999
zxstty <zhaojiaqi18@huawei.com>
2025-02-06 11:39:30 +0800

    flow相关的router、manager、utils调整

--- apps/routers/flow.py
+++ apps/routers/flow.py
@@ -2,7 +2,7 @@
 
 Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
 """
-from fastapi import APIRouter, Depends, status, Path, Query, Body
+from fastapi import APIRouter, Depends, status, Query, Body
 from fastapi.responses import JSONResponse
 from typing import Annotated, Optional
 
@@ -28,31 +28,30 @@ router = APIRouter(
 @router.get("/service", response_model=NodeMetaDataListRsp, responses={
     status.HTTP_404_NOT_FOUND: {"model": ResponseData},
 })
-async def get_node_metadatas(
-    user_sub: Annotated[str, Depends(get_user)],
-    page: int = Query(...),
-    page_size: int = Query(..., alias="pageSize"),
+async def get_services(
+    user_sub: Annotated[str, Depends(get_user)]
 ):
     """获取用户可访问的节点元数据所在服务的信息"""
-    result = await FlowManager.get_node_meta_datas_by_service_id(user_sub, page, page_size)
-    if result is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=ResponseData(
+    services = await FlowManager.get_service_by_user_id(user_sub)
+    if services is None:
+        return NodeServiceListRsp(
             code=status.HTTP_404_NOT_FOUND,
-            message="节点元数据所在服务信息不存在",
-            result={},
-        ).model_dump(exclude_none=True, by_alias=True))
-    return JSONResponse(status_code=status.HTTP_200_OK, content=NodeServiceListRsp(
+            message="未找到符合条件的服务",
+            result=NodeServiceListMsg(services=[])
+        )
+
+    return NodeServiceListRsp(
         code=status.HTTP_200_OK,
         message="节点元数据所在服务信息获取成功",
-        result=NodeServiceListMsg(total=result[0], services=result[1])
-    ).model_dump(exclude_none=True, by_alias=True))
+        result=NodeServiceListMsg(services=services)
+    )
 
 
 @router.get("/service/node", response_model=NodeMetaDataListRsp, responses={
     status.HTTP_403_FORBIDDEN: {"model": ResponseData},
     status.HTTP_404_NOT_FOUND: {"model": ResponseData}
 })
-async def get_flow(
+async def get_node_metadatas(
     user_sub: Annotated[str, Depends(get_user)],
     service_id: int = Query(..., alias="serviceId")
 ):
@@ -66,15 +65,15 @@ async def get_flow(
 
     result = await FlowManager.get_node_meta_datas_by_service_id(service_id)
     if result is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=ResponseData(
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=NodeMetaDataListRsp(
             code=status.HTTP_404_NOT_FOUND,
             message="服务下节点元数据获取失败",
-            result={},
+            result=NodeMetaDataListMsg(serviceId=service_id),
         ).model_dump(exclude_none=True, by_alias=True))
     return JSONResponse(status_code=status.HTTP_200_OK, content=NodeMetaDataListRsp(
         code=status.HTTP_200_OK,
         message="服务下节点元数据获取成功",
-        result=NodeMetaDataListMsg(node_meta_datas=result)
+        result=NodeMetaDataListMsg(serviceId=service_id, nodeMetaDatas=result)
     ).model_dump(exclude_none=True, by_alias=True))
 
 
@@ -89,17 +88,17 @@ async def get_flow(
 ):
     """获取流拓扑结构"""
     if not await AppManager.validate_user_app_access(user_sub, app_id):
-        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content=ResponseData(
+        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content=FlowStructureGetRsp(
             code=status.HTTP_403_FORBIDDEN,
             message="用户没有权限访问该流",
-            result={},
+            result=FlowStructureGetMsg(),
         ).model_dump(exclude_none=True, by_alias=True))
     result = await FlowManager.get_flow_by_app_and_flow_id(app_id, flow_id)
     if result is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=ResponseData(
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=FlowStructureGetRsp(
             code=status.HTTP_404_NOT_FOUND,
             message="应用下流程获取失败",
-            result={},
+            result=FlowStructureGetMsg(),
         ).model_dump(exclude_none=True, by_alias=True))
     return JSONResponse(status_code=status.HTTP_200_OK, content=FlowStructureGetRsp(
         code=status.HTTP_200_OK,
@@ -122,27 +121,36 @@ async def put_flow(
     put_body: PutFlowReq = Body(...)
 ):
     """修改流拓扑结构"""
-    if not await AppManager.validate_user_app_access(user_sub, app_id):
-        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content=ResponseData(
+    if not await AppManager.validate_app_belong_to_user(user_sub, app_id):
+        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content=FlowStructurePutRsp(
             code=status.HTTP_403_FORBIDDEN,
             message="用户没有权限访问该流",
-            result={},
+            result=FlowStructurePutMsg(),
         ).model_dump(exclude_none=True, by_alias=True))
+    put_body.flow=await FlowService.remove_excess_structure_from_flow(put_body.flow)
     if topology_check:
         await FlowService.validate_flow_connectivity(put_body.flow)
     await FlowService.validate_flow_illegal(put_body.flow)
     result = await FlowManager.put_flow_by_app_and_flow_id(app_id, flow_id, put_body.flow, put_body.focus_point)
     if result is None:
-        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=ResponseData(
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=FlowStructurePutRsp(
             code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            message="应用下流程更新失败",
-            result={},
+            message="应用下流更新失败",
+            result=FlowStructurePutMsg(),
+        ).model_dump(exclude_none=True, by_alias=True))
+    flow,_=await FlowManager.get_flow_by_app_and_flow_id(app_id, flow_id)
+    if flow is None:
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=FlowStructurePutRsp(
+            code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            message="应用下流更新后获取失败",
+            result=FlowStructurePutMsg(),
         ).model_dump(exclude_none=True, by_alias=True))
     return JSONResponse(status_code=status.HTTP_200_OK, content=FlowStructurePutRsp(
         code=status.HTTP_200_OK,
-        message="应用下流程更新成功",
-        result=FlowStructurePutMsg(flow_id=result)
+        message="应用下流更新成功",
+        result=FlowStructurePutMsg(flow=flow)
     ).model_dump(exclude_none=True, by_alias=True))
+
 
 
 @router.delete("", response_model=FlowStructureDeleteRsp, responses={
@@ -154,21 +162,21 @@ async def delete_flow(
     flow_id: str = Query(..., alias="flowId")
 ):
     """删除流拓扑结构"""
-    if not await AppManager.validate_user_app_access(user_sub, app_id):
-        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content=ResponseData(
+    if not await AppManager.validate_app_belong_to_user(user_sub, app_id):
+        return JSONResponse(status_code=status.HTTP_403_FORBIDDEN, content=FlowStructureDeleteRsp(
             code=status.HTTP_403_FORBIDDEN,
             message="用户没有权限访问该流",
-            result={},
+            result=FlowStructureDeleteMsg(),
         ).model_dump(exclude_none=True, by_alias=True))
     result = await FlowManager.delete_flow_by_app_and_flow_id(app_id, flow_id)
     if result is None:
-        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=ResponseData(
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content=FlowStructureDeleteRsp(
             code=status.HTTP_404_NOT_FOUND,
             message="应用下流程删除失败",
-            result={},
+            result=FlowStructureDeleteMsg(),
         ).model_dump(exclude_none=True, by_alias=True))
     return JSONResponse(status_code=status.HTTP_200_OK, content=FlowStructureDeleteRsp(
         code=status.HTTP_200_OK,
         message="应用下流程删除成功",
-        result=FlowStructureDeleteMsg(flow_id=result)
+        result=FlowStructureDeleteMsg(flowId=result)
     ).model_dump(exclude_none=True, by_alias=True))
--- apps/utils/flow.py
+++ apps/utils/flow.py
@@ -2,19 +2,48 @@
 
 Copyright (c) Huawei Technologies Co., Ltd. 2023-2024. All rights reserved.
 """
-from typing import Tuple, List
-
 from apps.constants import LOGGER
 from apps.entities.enum_var import NodeType
-from apps.entities.flow_topology import ServiceItem, NodeMetaDataItem, FlowItem, NodeItem, EdgeItem, PositionItem
+from apps.entities.flow_topology import FlowItem
 
 
 class FlowService:
     @staticmethod
+    async def remove_excess_structure_from_flow(flow_item: FlowItem) -> FlowItem:
+        node_to_branches = dict()
+        branch_illegal_chars='.'
+        for node in flow_item.nodes:
+            node_to_branches[node.node_id] = set()
+            if node.type == NodeType.CHOICE.value:
+                if 'choices' not in node.parameters.keys():
+                    node.parameters['choices'] = []
+                for branch in node.parameters['choices']:
+                    if branch['branch'] in node_to_branches[node.node_id]:
+                        LOGGER.error(msg="分支id重复")
+                        raise Exception(f"节点{node.name}的分支{branch['branch']}重复")
+                    for illegal_char in branch_illegal_chars:
+                        if illegal_char in branch['branch']:
+                            LOGGER.error(msg="分支名称中含有非法字符")
+                            raise Exception(f"节点{node.name}的分支{branch['branch']}名称中含有非法字符")
+                    node_to_branches[node.node_id].add(branch['branch'])
+            else:
+                node_to_branches[node.node_id].add('')
+        new_edges_items = []
+        for edge in flow_item.edges:
+            if edge.source_node not in node_to_branches.keys():
+                continue
+            if edge.target_node not in node_to_branches.keys():
+                continue
+            if edge.branch_id not in node_to_branches[edge.source_node]:
+                continue
+            new_edges_items.append(edge)
+        flow_item.edges = new_edges_items
+        return flow_item
+
+    @staticmethod
     async def validate_flow_illegal(flow_item: FlowItem) -> None:
         node_id_set = set()
         edge_id_set = set()
-        node_to_branches = dict()
         edge_to_branch = dict()
         num_of_start_node = 0
         num_of_end_node = 0
@@ -33,19 +62,10 @@ class FlowService:
             if node.type == NodeType.END.value:
                 num_of_end_node += 1
                 id_of_end_node = node.node_id
-            node_to_branches[node.node_id] = set()
-            if node.type == NodeType.CHOICE.value:
-                for branch in node.parameters['choices']:
-                    if branch.branch in node_to_branches[node.node_id]:
-                        LOGGER.error(msg=f"分支id重复")
-                        raise Exception(f"节点{node.name}的分支{branch.branch }重复")
-                    node_to_branches[node.node_id].add(branch.branch)
-            else:
-                node_to_branches[node.node_id].add('')
         if num_of_start_node != 1 or num_of_end_node != 1:
             LOGGER.error(msg="起始节点和终止节点数量不为1")
             raise Exception("起始节点和终止节点数量不为1")
-        for edge in flow_item.edeges:
+        for edge in flow_item.edges:
             if edge.edge_id in edge_id_set:
                 LOGGER.error(msg="边id重复")
                 raise Exception(f"边{edge.edge_id}的id重复")
@@ -53,29 +73,20 @@ class FlowService:
             if edge.source_node == edge.target_node:
                 LOGGER.error(msg="边起始节点和终止节点相同")
                 raise Exception(f"边{edge.edge_id}的起始节点和终止节点相同")
-            if edge.source_node not in node_id_set:
-                LOGGER.error(msg=f"边{edge.edge_id}的起始节点{edge.source_node}不存在")
-                raise Exception(f"边{edge.edge_id}的起始节点{edge.source_node}不存在")
-            if edge.target_node not in node_id_set:
-                LOGGER.error(msg=f"边{edge.edge_id}的终止节点{edge.target_node}不存在")
-                raise Exception(f"边{edge.edge_id}的终止节点{edge.target_node}不存在")
-            if edge.branch_id not in node_to_branches[edge.source_node]:
-                LOGGER.error(msg=f"边{edge.edge_id}的分支{edge.branch_id}不存在")
-                raise Exception(f"边{edge.edge_id}的分支{edge.branch_id}不存在")
+            if edge.source_node not in edge_to_branch:
+                edge_to_branch[edge.source_node] = set()
             if edge.branch_id in edge_to_branch[edge.source_node]:
                 LOGGER.error(msg=f"边{edge.edge_id}的分支{edge.branch_id}重复")
                 raise Exception(f"边{edge.edge_id}的分支{edge.branch_id}重复")
-            if edge.source_node not in edge_to_branch:
-                edge_to_branch[edge.source_node] = set()
-            edge_to_branch[edge.source_node].add(edge.edge_id)
+            edge_to_branch[edge.source_node].add(edge.branch_id)
             node_in_degrees[edge.target_node] = node_in_degrees.get(
                 edge.target_node, 0) + 1
             node_out_degrees[edge.source_node] = node_out_degrees.get(
                 edge.source_node, 0) + 1
-        if node_in_degrees[id_of_start_node] != 0:
+        if id_of_start_node in node_in_degrees.keys() and node_in_degrees[id_of_start_node] != 0:
             LOGGER.error(msg=f"起始节点{id_of_start_node}的入度不为0")
             raise Exception(f"起始节点{id_of_start_node}的入度不为0")
-        if node_out_degrees[id_of_end_node] != 0:
+        if id_of_end_node in node_out_degrees.keys() and node_out_degrees[id_of_end_node] != 0:
             LOGGER.error(msg=f"终止节点{id_of_end_node}的出度不为0")
             raise Exception(f"终止节点{id_of_end_node}的出度不为0")
 
@@ -90,7 +101,7 @@ class FlowService:
                 id_of_start_node = node.node_id
             if node.type == NodeType.END.value:
                 id_of_end_node = node.node_id
-        for edge in flow_item.edeges:
+        for edge in flow_item.edges:
             node_in_degrees[edge.target_node] = node_in_degrees.get(
                 edge.target_node, 0) + 1
             node_out_degrees[edge.source_node] = node_out_degrees.get(
