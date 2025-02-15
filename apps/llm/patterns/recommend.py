711612426eb303996cbdefd3e669371ad606b7a0
z30057876 <zhouweitong@huawei.com>
2025-02-15 10:25:22 +0800

    修正上下文填充

--- apps/llm/patterns/recommend.py
+++ apps/llm/patterns/recommend.py
@@ -15,18 +15,18 @@ class Recommend(CorePattern):
     """系统提示词"""
 
     user_prompt: str = r"""
-        根据对话上文、工具描述和用户倾向，生成预测问题。
+        根据上面的历史对话、结合给出的工具描述和用户倾向，生成预测问题。
 
         信息说明：
-        - [Empty]标识空信息，如“工具描述: [Empty]”表示当前未使用工具。
+        - [Empty]的含义是“空信息”，如“工具描述: [Empty]”表示当前未使用工具。
         - 历史提问信息为背景参考作用，最多提供4条。
 
         生成时需要遵循的要求：
         1. 从用户角度生成预测问题。
         2. 预测问题应为疑问句或祈使句，必须少于30字。
-        3. 预测问题应优先贴合工具描述，特别是工具描述与背景或倾向无关时。
-        5. 预测问题必须精简，不得输出非必要信息，不得输出除问题以外的文字。
-        6. 预测问题不得与“用户历史提问”重复或相似。
+        3. 预测问题应优先贴合工具描述，特别是工具描述与历史对话或用户倾向无关时。
+        4. 预测问题必须精简，不得输出非必要信息，不得输出除问题以外的文字。
+        5. 预测问题不得与“用户历史提问”重复或相似。
 
         ==以下是一个例子==
         工具描述：调用API，查询天气数据
@@ -79,11 +79,10 @@ class Recommend(CorePattern):
         user_input = self.user_prompt.format(
             action_description=action_description,
             history_questions=history_questions,
-            recent_question=kwargs["recent_question"],
             user_preference=user_preference,
         )
 
-        messages = [
+        messages = kwargs["recent_question"] + [
             {"role": "system", "content": self.system_prompt},
             {"role": "user", "content": user_input},
         ]
