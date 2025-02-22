f618a10a3c6e2b25a387abe8c110f77d64e87081
z30057876 <zhouweitong@huawei.com>
2025-02-22 20:14:54 +0800

    增加常量

--- apps/constants.py
+++ apps/constants.py
@@ -13,11 +13,15 @@ SLIDE_WINDOW_TIME = 60
 # 滑动窗口限流 最大请求数
 SLIDE_WINDOW_QUESTION_COUNT = 10
 # API Call 最大返回值长度（字符）
-MAX_API_RESPONSE_LENGTH = 4096
+MAX_API_RESPONSE_LENGTH = 8192
 # Scheduler最大历史轮次
 MAX_SCHEDULER_HISTORY_SIZE = 3
 # 语义接口目录中工具子目录
 CALL_DIR = "call"
+# 语义接口目录中服务子目录
+SERVICE_DIR = "service"
+# 语义接口目录中应用子目录
+APP_DIR = "app"
 # 日志记录器
 LOGGER = logging.getLogger("ray")
 
