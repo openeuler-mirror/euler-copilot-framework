4e09360d8b4eabf6e66595373119ee0f9f4c095e
方博 <1016318004@qq.com>
2025-02-24 17:30:25 +0800

    user去除无用部分

--- apps/routers/user.py
+++ apps/routers/user.py
@@ -1,24 +1,16 @@
 
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, HTTPException, status
-from fastapi.responses import JSONResponse, StreamingResponse
+from fastapi import APIRouter, Depends, status
+from fastapi.responses import JSONResponse
 
-from apps.common.queue import MessageQueue
-from apps.common.wordscheck import WordsCheck
-from apps.constants import LOGGER
 from apps.dependency import (
-    get_session,
     get_user,
     verify_csrf_token,
     verify_user,
 )
-from apps.entities.request_data import RequestData
-from apps.entities.response_data import ResponseData, UserGetMsp, UserGetRsp
+from apps.entities.response_data import UserGetMsp, UserGetRsp
 from apps.entities.user import UserInfo
-from apps.manager.appcenter import AppCenterManager
-from apps.scheduler.scheduler import Scheduler
-from apps.service.activity import Activity
 from apps.manager.user import UserManager
 
 router = APIRouter(
@@ -29,7 +21,6 @@ router = APIRouter(
 @router.get("", dependencies=[Depends(verify_csrf_token), Depends(verify_user)])
 async def chat(
     user_sub: Annotated[str, Depends(get_user)],
-    session_id: Annotated[str, Depends(get_session)],
 ) -> JSONResponse:
     """查询所有用户接口"""
     user_list = await UserManager.get_all_user_sub()
