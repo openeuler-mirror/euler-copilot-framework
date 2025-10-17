1dd9fb472a22ad4b265090dd9b64e096efaf28a4
z30057876 <zhouweitong@huawei.com>
2025-10-17 10:58:39 +0800

    修正初始化逻辑

--- apps/models/task.py
+++ apps/models/task.py
@@ -67,7 +67,7 @@ class Task(Base):
     """任务"""
 
     __tablename__ = "framework_task"
-    userSub: Mapped[str] = mapped_column(String(255), ForeignKey("framework_user.sub"))  # noqa: N815
+    userSub: Mapped[str] = mapped_column(String(255), ForeignKey("framework_user.userSub"))  # noqa: N815
     """用户ID"""
     conversationId: Mapped[uuid.UUID | None] = mapped_column(  # noqa: N815
         UUID(as_uuid=True), ForeignKey("framework_conversation.id"), nullable=True,
